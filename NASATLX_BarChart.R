103875c38db5e860adc0177be112d0b16e717e37
JellyFishKid1 <hbaez@umich.edu>
2024-05-29 17:33:03 -0400

    fixed nasa tlx

--- NASATLX_BarChart.R
+++ NASATLX_BarChart.R
@@ -2,20 +2,21 @@ ColorsByCondition <- c("#f8766d", "#619cff", "#00ba38")
 
 ## WORKING #####################################################################
 # boxplot for averages results with the simplified NASATLX csv
-NASATLX<-read.csv(file="NASATLX.csv") 
-NASA <- data.frame(
-  Categories=c(NASATLX[, c(1)] ),
+NASATLX<-read.csv(file="B Study_NASA-TLX.csv") 
+NASAOrg <- data.frame(
+  Participants=c(NASATLX[, c(1)] ),
   Condition=c(NASATLX[, c(2)] ),
-  AverageScore=c(NASATLX[, c(3)])
+  Category=c(NASATLX[, c(3)]),
+  Score=c(NASATLX[, c(4)])
 )
-
 library(ggplot2)
-ggplot(NASA, aes(x = Categories, y = AverageScore, fill = Condition)) + expand_limits(y=c(0, 7)) +
-  geom_col(position = position_dodge()) + scale_fill_manual(values = ColorsByCondition) +
-  labs(title="NASA-TLX Averaged Results", x="Category", y="Average Score")+
-  theme(plot.title = element_text(hjust=0.5)) + scale_y_continuous(breaks = seq(0, 7, 1)) +
-  geom_text(aes(label = AverageScore),colour = "white", size = 4, vjust = 1.5, position = position_dodge(.9))
-
+summary_stats_NASA <- with(NASAOrg, tapply(Score, list(Condition, Category), summary))
+# grouped boxplot
+ggplot(NASAOrg, aes(x=Category, y=Score, fill=Condition)) + 
+  geom_boxplot() +
+  scale_fill_manual(values=c("#619cff", "#00ba38")) +
+  labs(title = "NASA-TLX Results") +
+  theme(plot.title = element_text(hjust=0.5)) 
 ################################################################################
 # Using all the participant results to graph
 Results<-read.csv(file="ParticipantResults.csv") 
@@ -60,7 +61,7 @@ ggplot(averages, aes(x = turn_number, y = score, color = Condition)) +
 ## Oneway Communication Plot
 OneWay_data <- sorted_DTS[sorted_DTS$Condition == "OneWay", ]
 # Plot scatter plot
-blue_shades <- c("#619cff", "#4682d4", "#2f77ca", "#177cc0", "#086ea6", "#005a7a")
+blue_shades <- c("#619cff", "#4682d4", "#2f77ca", "#177cc0", "#086ea6", "#005a7a", "#619cff99")
 # Plot scatterplot with connected lines, each a different shade of blue per participant
 ggplot(OneWay_data, aes(x = turn_number, y = score, color = as.factor(participantNumber))) +
   geom_point(size=4) +
@@ -72,7 +73,7 @@ ggplot(OneWay_data, aes(x = turn_number, y = score, color = as.factor(participan
 ## Twoway Communication Plot
 TwoWay_data <- sorted_DTS[sorted_DTS$Condition == "TwoWay", ]
 # Plot scatter plot
-green_shades <- c("#00ba38", "#12c94b", "#24d45d", "#36df70", "#49ea82", "#5bf494")
+green_shades <- c("#00ba38", "#12c94b", "#24d45d", "#36df70", "#49ea82", "#5bf494", "#2ca34d")
 # Plot scatterplot with connected lines, each a different shade of blue per participant
 ggplot(TwoWay_data, aes(x = turn_number, y = score, color = as.factor(participantNumber))) +
   geom_point(size=4) +
@@ -85,3 +86,26 @@ ggplot(TwoWay_data, aes(x = turn_number, y = score, color = as.factor(participan
 #Data from matlab performance metrics 
 PerformMetric<-read.csv(file="PerformanceMetrics.csv") 
 
+
+################################################################################
+################################################################################
+# Pie chart for customization 
+# Sample data
+labels <- c("Direction Indicated by Inflation", "Direction Indicated by Deflation")
+sizes <- c(71.42, 28.57)  # Percentages
+
+# Create pie chart
+piechart <- pie(sizes, labels = labels, main = "Inflation Type")
+
+# Calculate percentages
+percentages <- round(100 * sizes / sum(sizes), 1)
+
+# Add values to the pie chart
+text(
+  x = piechart$x, 
+  y = piechart$y, 
+  labels = percentages,
+  cex = 0.8,
+  pos = 3,
+  col = "white"
+)
